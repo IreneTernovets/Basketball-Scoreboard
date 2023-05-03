c5feb6d46543d325d76b9dbef65e3069e7ee7f23
Irene <terna.legend@gmail.com>
2023-05-03 14:16:40 +0200

    implemented adding numbers fucntion and reset game function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function addNumber(addArg) {
-  console.log(addArg);
+let homeScoreWindow = document.getElementById("home-score");
+let homeScore = 0;
+
+let guestScoreWindow = document.getElementById("guest-score");
+let guestScore = 0;
+
+function addNumberHome(addArg) {
+  homeScore += addArg;
+  homeScoreWindow.innerText = homeScore;
+}
+
+function addNumberGuest(addArg) {
+  guestScore += addArg;
+  guestScoreWindow.innerText = guestScore;
 }
